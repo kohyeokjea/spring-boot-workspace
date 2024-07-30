b983761e7e33c5cb8587a0b3907054b597d2584d
kohyeokjea <kohj0686@naver.com>
2024-07-30 21:58:48 +0900

    스프링 부트 4일차

---
+++ ex07_form/src/main/java/com/example/ex07_form/Member.java
@@ -0,0 +1,22 @@
+package com.example.ex07_form;
+
+public class Member {
+    private String id;
+    private String name;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
