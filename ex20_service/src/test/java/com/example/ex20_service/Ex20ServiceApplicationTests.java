5b7281c07ed5acf6f2d2f3e5fb2a96ab38742cdd
kohyeokjea <kohj0686@naver.com>
2024-08-08 21:51:34 +0900

    스프링 부트  12일차

---
+++ ex20_service/src/test/java/com/example/ex20_service/Ex20ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ex20_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex20ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
